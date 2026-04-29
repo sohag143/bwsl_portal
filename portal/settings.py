a283e96d3f57fd5bfc7978aa2cc655f9d1f38880
আপনার নাম < >
2026-04-29 12:51:22 +0600

    Ready for hosting

--- portal/settings.py
+++ portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o@pa(iqj#w3$+mcfag8c_3y8uboe^60v!03bh2_ly8q6e1r6!a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.bwsl-portal.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
